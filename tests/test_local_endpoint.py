9eb6a8a9d2d13ad83e466dfb0cd7f7cdb74a7e84
kongo09 <kongo09@gmail.com>
2021-12-28 00:56:47 +0100

    mark local tests as possible fails

--- tests/test_local_endpoint.py
+++ tests/test_local_endpoint.py
@@ -1,21 +1,26 @@
 import aiohttp
+import pytest
 
 from dell_printer_parser.const import STATUS_URL, INFORMATION_URL, PRINT_VOLUME_URL
 
 LOCAL_TEST_IP = "192.168.0.20"
 
+@pytest.mark.xfail
 async def test_endpoint_status():
     """Test a status endpoint"""
     await _test_endpoint(STATUS_URL)
 
+@pytest.mark.xfail
 async def test_endpoint_information():
     """Test a status endpoint"""
     await _test_endpoint(INFORMATION_URL)
 
+@pytest.mark.xfail
 async def test_endpoint_print_volume():
     """Test a status endpoint"""
     await _test_endpoint(PRINT_VOLUME_URL)
 
+@pytest.mark.xfail
 async def _test_endpoint(url: str):
     """Test a given endpoint is returning data"""
     async with aiohttp.ClientSession() as session:
