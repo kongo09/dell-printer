cda4a76b06e5ede5e35592a90151f6b09397a203
kongo09 <kongo09@gmail.com>
2021-12-28 10:50:20 +0100

    skip tests that only work locally

--- tests/test_local_endpoint.py
+++ tests/test_local_endpoint.py
@@ -5,22 +5,21 @@ from dell_printer_parser.const import STATUS_URL, INFORMATION_URL, PRINT_VOLUME_
 
 LOCAL_TEST_IP = "192.168.0.20"
 
-@pytest.mark.xfail
+@pytest.mark.skip(reason="only works locally")
 async def test_endpoint_status():
     """Test a status endpoint"""
     await _test_endpoint(STATUS_URL)
 
-@pytest.mark.xfail
+@pytest.mark.skip(reason="only works locally")
 async def test_endpoint_information():
     """Test a status endpoint"""
     await _test_endpoint(INFORMATION_URL)
 
-@pytest.mark.xfail
+@pytest.mark.skip(reason="only works locally")
 async def test_endpoint_print_volume():
     """Test a status endpoint"""
     await _test_endpoint(PRINT_VOLUME_URL)
 
-@pytest.mark.xfail
 async def _test_endpoint(url: str):
     """Test a given endpoint is returning data"""
     async with aiohttp.ClientSession() as session:
